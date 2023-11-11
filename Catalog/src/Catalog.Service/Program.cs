180c0d99510094c1e12aea1c36b7030cf24c6fb2
iTzNightmare <dustin.arbeiter@gmx.de>
2023-11-11 03:20:40 +0100

    ✨ bind mongodb to the system

--- Catalog/src/Catalog.Service/Program.cs
+++ Catalog/src/Catalog.Service/Program.cs
@@ -1,9 +1,15 @@
+using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.Serializers;
+using MongoDB.Bson;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers(options => {
     options.SuppressAsyncSuffixInActionNames = false; // Prevents MVC to Remove the suffix "Async" in the Controller (e.g ItemsController.cs)
+    BsonSerializer.RegisterSerializer(new GuidSerializer(BsonType.String)); // Serializer for GUID ID Data => Makes the ID in the DB more readable
+    BsonSerializer.RegisterSerializer(new DateTimeOffsetSerializer(BsonType.String)); // Serializer for DateTime Data => Makes the DateTimne in the DB more readable
+
 });
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
