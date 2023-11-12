2b64efd387249c0f3558d32e39009abdf903bc99
iTzNightmare <dustin.arbeiter@gmx.de>
2023-11-12 17:33:11 +0100

    ♻️ refactor and simplify code

--- Catalog/src/Catalog.Service/Program.cs
+++ Catalog/src/Catalog.Service/Program.cs
@@ -7,30 +7,16 @@ using Catalog.Service.Repositories;
 using Catalog.Service.Entities;
 
 var builder = WebApplication.CreateBuilder(args);
-
 ServiceSettings serviceSettings;
 
- builder.Configuration.AddJsonFile("appsettings.json");
+builder.Configuration.AddJsonFile("appsettings.json");
 serviceSettings = builder.Configuration.GetSection(nameof(ServiceSettings)).Get<ServiceSettings>();
-
-builder.Services.AddSingleton(serviceProvider => {
-    var mongoDbSettings = builder.Configuration.GetSection(nameof(MongoDbSettings)).Get<MongoDbSettings>();
-    var mongoClient = new MongoClient(mongoDbSettings.ConnectionString);
-    return mongoClient.GetDatabase(serviceSettings.ServiceName);
-});
-
-
-    builder.Services.AddSingleton<IRepository<Item>>(serviceProvider => 
-    {
-        var database = serviceProvider.GetService<IMongoDatabase>();
-        return new MongoRepository<Item>(database, "items");
-    });
+builder.Services.AddMongo().AddMongoRepository<Item>("items");
 
 // Add services to the container.
 builder.Services.AddControllers(options => {
     options.SuppressAsyncSuffixInActionNames = false; // Prevents MVC to Remove the suffix "Async" in the Controller (e.g ItemsController.cs)
-    BsonSerializer.RegisterSerializer(new GuidSerializer(BsonType.String)); // Serializer for GUID ID Data => Makes the ID in the DB more readable
-    BsonSerializer.RegisterSerializer(new DateTimeOffsetSerializer(BsonType.String)); // Serializer for DateTime Data => Makes the DateTimne in the DB more readable
+    
 });
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
