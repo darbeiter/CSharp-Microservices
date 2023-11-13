cb4ff6da7963cd06b03ecbb6f2ef8ffa1b957df4
iTzNightmare <dustin.arbeiter@gmx.de>
2023-11-13 13:14:15 +0100

    ✨ including timeouts for inter communication

--- Inventory/src/Inventory.Service/Program.cs
+++ Inventory/src/Inventory.Service/Program.cs
@@ -1,6 +1,8 @@
+using Polly;
 using Common.MongoDB;
 using Inventory.Service.Clients;
 using Inventory.Service.Entities;
+using Polly.Timeout;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,9 +13,14 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddMongo().AddMongoRepository<InventoryItem>("inventoryitems");
+Random jitterer = new Random();
 builder.Services.AddHttpClient<CatalogClient>(client => {
     client.BaseAddress = new Uri("https://localhost:7108");
-});
+}).AddTransientHttpErrorPolicy(builder => builder.Or<TimeoutRejectedException>().WaitAndRetryAsync(
+    5,
+    retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt))
+    + TimeSpan.FromMilliseconds(jitterer.Next(0, 1000))
+)).AddPolicyHandler(Policy.TimeoutAsync<HttpResponseMessage>(1));
 
 var app = builder.Build();
 
