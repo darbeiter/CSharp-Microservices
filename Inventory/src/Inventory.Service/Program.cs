463db883423e26177a6172ed972fb6e266adda76
iTzNightmare <dustin.arbeiter@gmx.de>
2023-11-13 20:49:06 +0100

    ✨ create consumer for rabbitmq

--- Inventory/src/Inventory.Service/Program.cs
+++ Inventory/src/Inventory.Service/Program.cs
@@ -3,6 +3,7 @@ using Common.MongoDB;
 using Inventory.Service.Clients;
 using Inventory.Service.Entities;
 using Polly.Timeout;
+using Common.MassTransit;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,20 +13,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddMongo().AddMongoRepository<InventoryItem>("inventoryitems");
-Random jitterer = new Random();
-builder.Services.AddHttpClient<CatalogClient>(client => {
-    client.BaseAddress = new Uri("https://localhost:7108");
-}).AddTransientHttpErrorPolicy(builder => builder.Or<TimeoutRejectedException>().WaitAndRetryAsync(
-    5,
-    retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt))
-    + TimeSpan.FromMilliseconds(jitterer.Next(0, 1000))
-))
-.AddTransientHttpErrorPolicy(builder => builder.Or<TimeoutRejectedException>().CircuitBreakerAsync(
-    3,
-    TimeSpan.FromSeconds(15)
-))
-.AddPolicyHandler(Policy.TimeoutAsync<HttpResponseMessage>(1));
+builder.Services.AddMongo()
+.AddMongoRepository<InventoryItem>("inventoryitems")
+.AddMongoRepository<CatalogItem>("catalogitems")
+.AddMassTransitWithRabbitMq();
+
+AddCatalogClient(builder);
 
 var app = builder.Build();
 
@@ -43,3 +36,21 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+static void AddCatalogClient(WebApplicationBuilder builder)
+{
+    Random jitterer = new Random();
+    builder.Services.AddHttpClient<CatalogClient>(client =>
+    {
+        client.BaseAddress = new Uri("https://localhost:7108");
+    }).AddTransientHttpErrorPolicy(builder => builder.Or<TimeoutRejectedException>().WaitAndRetryAsync(
+        5,
+        retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt))
+        + TimeSpan.FromMilliseconds(jitterer.Next(0, 1000))
+    ))
+    .AddTransientHttpErrorPolicy(builder => builder.Or<TimeoutRejectedException>().CircuitBreakerAsync(
+        3,
+        TimeSpan.FromSeconds(15)
+    ))
+    .AddPolicyHandler(Policy.TimeoutAsync<HttpResponseMessage>(1));
+}
